6cac0b54e36226557f63bf88b612ac27e50d01cc
Vinicius Pereira <pereiravinao@MacBook-Pro-de-Vinicius.local>
2021-09-17 18:21:05 -0300

    Correção

--- src/pages/CartItems.jsx
+++ src/pages/CartItems.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class CartItems extends React.Component {
   render() {
     return (
-      <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+      <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
     );
   }
 }
