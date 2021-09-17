b62edea35aed1008f67187beeca28f0ee3444aaa
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-17 18:47:41 -0300

    Cria componente CategoriesBar

---
+++ src/components/CategoriesBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class InitialMsg extends Component {
+  render() {
+    return (
+      <div data-testid="category">
+        Barra de categorias!
+      </div>
+    );
+  }
+}
